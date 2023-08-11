429f7dd90677e2105c97d87655a6bd043811b2e5
novelus1 <krzysztof.truszkowski@outlook.com>
2023-08-11 20:52:51 +0200

    Add recently finished 8 kyu cwars

---
+++ Array times 2.js
@@ -0,0 +1,7 @@
+function maps(x) {
+  const result = x.map((xItem) => {
+    return xItem * 2;
+  });
+
+  return result;
+}
---
+++ Int from charAt(0).js
@@ -0,0 +1,6 @@
+function getAge(inputString) {
+  let age = inputString.charAt(0);
+  return Number(age);
+}
+
+
